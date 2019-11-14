9bed7dc737f51002e6b61a1a84f93b7b88810a13
Kukuh <kukuhsain@gmail.com>
2019-11-14 20:08:57 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) return val;
+  if (port >= 0) return port;
+  return false;
+}
+
+const expressApp = express();
+expressApp
+  .use(logger("dev"))
+  .use(express.json())
+  .use(express.urlencoded({extended: false}))
+  .use(cookieParser())
+  .use(express.static(path.join(__dirname, "public")));
+
+// Routing
+expressApp.use("/", indexRouter);
+
+const http = require("http");
+const port = normalizePort(process.env.PORT || "5000");
+const server = http.createServer(expressApp);
+server
+  .listen(port)
+  .on("error", error => console.error(error))
+  .on("listening", () => console.log("Listening on port " + port));
