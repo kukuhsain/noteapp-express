5928524bcc3d442eb4d0bd0ce9dfd6faa8676a03
Kukuh <kukuhsain@gmail.com>
2019-11-14 23:11:12 +0700

    Prepare to implement Typescript into the project

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const indexRouter = require("./routes/index");
+const indexRouter = require("./routes");
 const notesRouter = require("./routes/notes");
 
 function normalizePort(val) {
